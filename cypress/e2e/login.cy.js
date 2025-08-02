b9261ed9d075972401349fadd53a58ef8ad443cf
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-08-02 14:43:24 -0300

    adicionando beforeEach e commands

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,23 @@
 describe('login',()=>{
-    it('login com credenciais validas', ()=>{
-    cy.fixture('inputLogin.json').then((usuario)=>{
-    cy.loginValido(usuario.username, usuario.senha)})
+  beforeEach(()=>{
+    cy.visitarPagina()
+
+
+
+  })
+
+  it('login com credenciais validas', ()=>{
+  cy.fixture('inputLogin.json').then((usuario)=>{
+  cy.loginValido(usuario.username, usuario.senha)})
+
+})
+
+  it('login com credenciais invalidas',()=>{
+      cy.loginInvalido('pedro','1234')
+      
+
 
   })
 })
+
+
