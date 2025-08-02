b0f2d6d53ebcf692f2250ec0fd2b5ac12840c772
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-08-02 14:59:30 -0300

    Adicionando scripts no package.json

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,22 +2,17 @@ describe('login',()=>{
   beforeEach(()=>{
     cy.visitarPagina()
 
-
-
   })
 
   it('login com credenciais validas', ()=>{
   cy.fixture('inputLogin.json').then((usuario)=>{
   cy.loginValido(usuario.username, usuario.senha)})
-
 })
 
   it('login com credenciais invalidas',()=>{
-      cy.loginInvalido('pedro','1234')
-      
-
-
+      cy.loginInvalido('pedro','1234')   
   })
+
 })
 
 
